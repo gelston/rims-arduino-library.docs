79ec583efccd7ed28840a43dc978a93e3c1efa99
Francis Gagnon <Francis Gagnon@localhost>
2013-06-25 14:55:06 -0400

    -modif typos - add URimsIdent and RimsIdent docs

--- html/search/functions_72.js
+++ html/search/functions_72.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['readkeysadc',['readKeysADC',['../class_u_i_rims.html#a9e3674e586ac9fdeaba02f5905b48994',1,'UIRims']]],
   ['rims',['Rims',['../class_rims.html#ac370295657d50fed766ccded3e7da76c',1,'Rims']]],
+  ['rimsident',['RimsIdent',['../class_rims_ident.html#a795c913c96e00b8cfb3fbc9e52f2fdf8',1,'RimsIdent']]],
   ['run',['run',['../class_rims.html#a60650cdc19193c465d47b1f3ad72502f',1,'Rims']]]
 ];
