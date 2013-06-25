9b33bdf27980467a87ab5ba37a84686cfbe4801c
Francis Gagnon <Francis Gagnon@localhost>
2013-06-25 16:39:11 -0400

    -docs main page

--- html/search/pages_72.js
+++ html/search/pages_72.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['recirculation_20infusion_20mash_20system_20library_20for_20arduino',['Recirculation infusion mash system library for arduino',['../index.html',1,'']]]
+  ['recirculation_20infusion_20mash_20system_20library_20for_20arduino',['Recirculation infusion mash system library for Arduino',['../index.html',1,'']]]
 ];
