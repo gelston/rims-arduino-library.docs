e13984412e1f09736770cfe34a6870cdb36c11cb
Francis Gagnon <Francis Gagnon@localhost>
2013-06-25 22:35:05 -0400

    -ring added

--- html/search/functions_72.js
+++ html/search/functions_72.js
@@ -3,5 +3,6 @@ var searchData=
   ['readkeysadc',['readKeysADC',['../class_u_i_rims.html#a9e3674e586ac9fdeaba02f5905b48994',1,'UIRims']]],
   ['rims',['Rims',['../class_rims.html#ac370295657d50fed766ccded3e7da76c',1,'Rims']]],
   ['rimsident',['RimsIdent',['../class_rims_ident.html#a795c913c96e00b8cfb3fbc9e52f2fdf8',1,'RimsIdent']]],
+  ['ring',['ring',['../class_u_i_rims.html#a528139cb2db1e7cde3d8daa6e579b8df',1,'UIRims']]],
   ['run',['run',['../class_rims.html#a60650cdc19193c465d47b1f3ad72502f',1,'Rims']]]
 ];
