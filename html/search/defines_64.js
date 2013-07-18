7fe9706665a591c43b82ec47ffa165d9d1362298
franckgaga2 <franckgaga2@gmail.com>
2013-07-18 17:53:49 -0400

    added multiple pid values

--- html/search/defines_64.js
+++ html/search/defines_64.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['defaultres1',['DEFAULTRES1',['../_rims_8h.html#aaf40faafca9bf2c60d4a0e5c98d3b091',1,'Rims.h']]],
   ['defaultsp',['DEFAULTSP',['../_rims_8h.html#ae9c41ce8eb83b29271bfe9fa42d8f476',1,'Rims.h']]],
-  ['defaulttime',['DEFAULTTIME',['../_rims_8h.html#a6c20fa161321bf04ecd94be0c3a64f4f',1,'Rims.h']]],
-  ['doublebatch',['DOUBLEBATCH',['../_rims_8h.html#a362e16cd2e842e24d36765e2992452b3',1,'Rims.h']]]
+  ['defaulttime',['DEFAULTTIME',['../_rims_8h.html#a6c20fa161321bf04ecd94be0c3a64f4f',1,'Rims.h']]]
 ];
--- html/search/defines_73.js
+++ html/search/defines_73.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['screenswitchtime',['SCREENSWITCHTIME',['../_rims_8h.html#a6a5c2983e6011e0d53647dee104a8293',1,'Rims.h']]],
-  ['simplebatch',['SIMPLEBATCH',['../_rims_8h.html#a8403ef076d5ae845ca32914090b651e1',1,'Rims.h']]],
   ['ssrwindowsize',['SSRWINDOWSIZE',['../_rims_8h.html#a847381f6bf45c8c398dfe6f2aff10986',1,'Rims.h']]]
 ];
