17442acb5383f4e5eef6cff221f376b556858611
francis <francis@francis-Inspiron-1545>
2013-07-31 01:40:09 -0400

    -forgot files

---
+++ html/search/functions_6c.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['lcdlight',['lcdLight',['../class_u_i_rims.html#a2eeef27f03378117d298794a44595006',1,'UIRims']]]
+];
---
+++ html/search/functions_74.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['timerrunningchar',['timerRunningChar',['../class_u_i_rims.html#a0102be91ebc9d14520528af0e2cbc577',1,'UIRims']]]
+];
