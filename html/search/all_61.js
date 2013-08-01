258a5521edb92dd4505a428089125e9177eb9c45
francis <francis@francis-Inspiron-1545>
2013-08-01 00:06:10 -0400

    - debug speaker, disconnected therm simplified

--- html/search/all_61.js
+++ html/search/all_61.js
@@ -1,5 +1,7 @@
 var searchData=
 [
+  ['alarmfreq',['ALARMFREQ',['../_u_i_rims_8h.html#aaa126dda9be2430228a74f71648227ff',1,'UIRims.h']]],
+  ['alarmlength',['ALARMLENGTH',['../_u_i_rims_8h.html#aeb9865e4e92e947cc98a244378841432',1,'UIRims.h']]],
   ['askmashwater',['askMashWater',['../class_u_i_rims.html#a2e34a0a4f7c240f618dcaf7b3c635c64',1,'UIRims']]],
   ['asksetpoint',['askSetPoint',['../class_u_i_rims.html#a580605afd235a7e6041f4d99aef4285c',1,'UIRims']]],
   ['asktime',['askTime',['../class_u_i_rims.html#a4d41f8d7a401cb9385d756d100fb3bea',1,'UIRims']]]
---
+++ html/search/defines_61.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['alarmfreq',['ALARMFREQ',['../_u_i_rims_8h.html#aaa126dda9be2430228a74f71648227ff',1,'UIRims.h']]],
+  ['alarmlength',['ALARMLENGTH',['../_u_i_rims_8h.html#aeb9865e4e92e947cc98a244378841432',1,'UIRims.h']]]
+];
