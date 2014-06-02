bef45e345c32e392069ff37de59eecb9e5d1b04d
francis <francis@localhost>
2014-06-01 22:51:48 -0400

    v2.0

--- html/search/all_63.js
+++ html/search/all_63.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['checkmemaccessmode',['checkMemAccessMode',['../class_rims.html#a2ee96dee2f90f42e88b1799affc585d7',1,'Rims']]],
   ['criticalflow',['CRITICALFLOW',['../_rims_8h.html#a3e4da66d776edb92ea6ebc0128749dc2',1,'Rims.h']]]
 ];
